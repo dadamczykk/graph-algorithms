4567333d2702af28452057ad066c38f93aa20aaf
Dominik Adamczyk <dominik.adamczyk1000@gmail.com>
2023-02-03 13:26:37 +0100

    added timer to tests

--- runtest.py
+++ runtest.py
@@ -1,33 +1,43 @@
 from os import listdir
 from os.path import isfile
 from dimacs import readSolution
+import time
 import os
 
 def runtest(function, directory, to_omit = []):
-    
+    all_time = time.time()
+
     has_uncorrect = False
-    
+
     if directory[-1] != "\\":
         directory += "\\"
 
     graphs = [name for name in listdir(directory) if name not in to_omit]
-    
+
     print("Testing files: ",  graphs)
 
     for graph in graphs:
+        
+        test_time = time.time()
         path = directory + graph
+        
         if not isfile(path):
             continue
         ans = int(readSolution(path))
+        
         print("\n============================\n")
         print("Filename: " + graph)
-        myans = function(path)
-        if ans != myans:
-            has_uncorrect = True
         
+        myans = function(path)
+
         print("Correct solution: ", ans)
         print("My solution:      ", myans)
         print("Answer is " + "correct" if myans == ans else "uncorrect")
-    
+        print(f"Time of execution: {time.time() - test_time:.4f}s", )
+        
+        if ans != myans:
+            has_uncorrect = True
+
     print("\n" + "Everything is OK!" if not has_uncorrect else "There are problems!")
-    return has_uncorrect
+    print(f"\nAll tests time: {time.time() - all_time:.4f}s")
+    return None
